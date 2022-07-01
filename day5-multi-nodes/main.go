f130403bd94ccdedc13d17c20e7090f644584ce2
liuyang89116 <liuyang89116@qq.com>
2022-06-30 22:51:04 -0700

    Add main and script

--- day5-multi-nodes/main.go
+++ day5-multi-nodes/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"example/geecache"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,42 +14,68 @@ var db = map[string]string{
 	"Sam":  "821",
 }
 
-/**
-简单测试：
-
-$ curl http://localhost:9999/_geecache/scores/Tom
-630
-
-$ curl http://localhost:9999/_geecache/scores/kkk
-kkk not exist
-*/
-func main() {
-	geecache.NewGroup("scores", 2<<10, geecache.GetterFunc(
+func createGroup() *geecache.Group {
+	return geecache.NewGroup("scores", 2<<10, geecache.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
 				return []byte(v), nil
 			}
-			return nil, fmt.Errorf("%s doesn't exits\n", key)
+			return nil, fmt.Errorf("%s doesn't exits", key)
 		},
 	))
-
-	addr := "localhost:9999"
-	httpPool := geecache.NewHttpPool(addr)
-	log.Println("geecache is running at", addr)
-	log.Fatal(http.ListenAndServe(addr, httpPool))
 }
 
-/**
-运行结果：
+func startCacheServer(addr string, addrs []string, gee *geecache.Group) {
+	peers := geecache.NewHttpPool(addr)
+	peers.Set(addrs...)
+	gee.RegisterPeers(peers)
+	log.Println("geecache is running at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
 
-2022/05/15 23:56:47 geecache is running at localhost:9999
-2022/05/15 23:57:19 [Server localhost:9999] GET /_geecache/scores/Tom/n
-2022/05/15 23:57:19 [SlowDB] search key Tom
-2022/05/15 23:57:34 [Server localhost:9999] GET /_geecache/scores/TTom/n
-2022/05/15 23:57:34 [SlowDB] search key TTom
-2022/05/15 23:57:39 [Server localhost:9999] GET /_geecache/scores/Tom/n
-2022/05/15 23:57:39 [GeeCache] hit
-2022/05/15 23:57:50 [Server localhost:9999] GET /_geecache/scores/Tom1/n
-2022/05/15 23:57:50 [SlowDB] search key Tom1
-*/
+func startAPIServer(apiAddr string, gee *geecache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			byteView, err := gee.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(byteView.ByteSlice())
+		}))
+
+	log.Println("frontend sever is running at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func main() {
+	var (
+		port int
+		api  bool
+	)
+
+	flag.IntVar(&port, "port", 9001, "Geecache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		9001: "http://localhost:9001",
+		9002: "http://localhost:9002",
+		9003: "http://localhost:9003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	gee := createGroup()
+	if api {
+		go startAPIServer(apiAddr, gee)
+	}
+	startCacheServer(addrMap[port], []string(addrs), gee)
+}
